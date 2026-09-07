28212481f8f2c29b31f7a18662c04c4e6c0e9374
Potato <132866506+alixd1085@users.noreply.github.com>
2026-09-07 20:31:23 +0300

    Project_streak v0.1
    - You can now store your streak and increase it whenever you run the app

--- Project_streak.py
+++ Project_streak.py
@@ -29,21 +29,25 @@ if not os.path.exists("streak data.json"):
 else:
     print("path already exists")
 
+
 with open('streak data.json', 'r') as r:
     data = json.load(r)
 
-if readable_days in data:
-    print('come back tommorow')
-else:
+previous_day = time.strftime("%Y/%m/%d", time.localtime(time.time() - 86400))
+
+if previous_day in data:
+    current_streak = int(data[previous_day]['Current_Streak'])
     current_streak += 1
+else:
+ current_streak += 1
 
-    data[readable_days] = {
-        "timestamp":readable_hours,
-        "App":PID,
-        "Current_Streak":current_streak
-    }
-    with open('streak data.json', 'w') as w:
-        json.dump(data, w, indent=4)
-
-print(data)
+data[readable_days] = {
+    "timestamp":readable_hours,
+    "App":PID,
+    "Current_Streak":current_streak
+}
+with open('streak data.json', 'w') as w:
+    json.dump(data, w, indent=4)
+        
 #===== Debug Zone ======
+print(data)
